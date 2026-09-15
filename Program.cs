77d5b6578e1be0b562632046d96c8bd3b5e5e7b6
Linus <wlinusolsson@gmail.com>
2026-09-15 14:58:22 +0200

    forth-commit

--- Program.cs
+++ Program.cs
@@ -29,44 +29,36 @@
 
 class Program
 {
+
+    static List<Person> profiles = new List<Person>();
+
+
+
+
     static void Main()
     {
 
-        Person person1 = new Person();
+        Person person = new Person();
 
         Console.Write("Skriv ditt namn: ");
-        person1.userName = Console.ReadLine();
+        person.userName = Console.ReadLine();
+        profiles.Add(person);
         Console.Write("Skriv in din Ålder: ");
-        person1.age = int.Parse(Console.ReadLine());
+        person.age = int.Parse(Console.ReadLine());
 
 
 
 
-        Person person2 = new Person();
-        person2.userName = "Bengt";
-        person2.age = 45;
-
-
-
-
-
-
-        Console.WriteLine("Hej :" + person1.userName + " du är : " + person1.age + " år gammal");
-        Console.WriteLine("Hej :" + person2.userName + " du är : " + person2.age + " år gammal");
 
     }
 }
 
-//Göra en class
 class Person
 {
     public string userName;
     public int age;
 
-
-
 }
-
 
 
 
