e1eb2aa80aebc8fb2b057f02937d327f394fb7ab
Linus <wlinusolsson@gmail.com>
2026-09-15 21:50:21 +0200

    sixth-Commit

--- Program.cs
+++ Program.cs
@@ -47,7 +47,8 @@ class Program
             Console.WriteLine("Välj ett av följande val: ");
             Console.WriteLine("1: Skapa en Profil ");
             Console.WriteLine("2: Se alla profiler: ");
-            Console.WriteLine("3: Avsluta");
+            Console.WriteLine("3: Redigera Profil: ");
+            Console.WriteLine("4: Avsluta");
             Console.WriteLine("-------------");
             string answer = Console.ReadLine();
 
@@ -72,6 +73,24 @@ class Program
                     Console.WriteLine($"Namn: {p.userName} Ålder: {p.age} ");
                 }
             }
+
+            else if (answer == "3")
+            {
+                for (int i = 0; i < profiles.Count; i++)
+                {
+                    Console.Write("Vilken profil vill du redigera?: ");
+                    Console.Write(profiles[i].userName + "\n");
+
+                    int choice = int.Parse(Console.ReadLine());
+
+
+                }
+
+            }
+
+            /// Jag behöver komma in i min lista för att välja ut ett objekt som har skapats och ska redigera det.
+
+
             else
             {
                 break;
