5a6bb892a5e09e816193953cfe639beea293d5af
Linus <wlinusolsson@gmail.com>
2026-09-15 13:16:04 +0200

    Second-Commit

--- Program.cs
+++ Program.cs
@@ -31,23 +31,6 @@
 
 //Göra en class
 
-class Student
-{
-    public string userName = "Inget namn";
-    public int age;
-}
 
-class bla
-{
 
-    static void Main()
-    {
-        Student student1 = new Student();
-        student1.userName = "Bengt";
 
-        Student student2 = new Student();
-
-        Console.WriteLine("student1: " + student1.userName);
-        Console.WriteLine("student 2: " + student2.userName);
-    }
-}
