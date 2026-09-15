26a8b3f90697fc32b995c0a6cc6bf1752f139865
Linus <wlinusolsson@gmail.com>
2026-09-15 19:49:17 +0200

    Fith-Commit

--- Program.cs
+++ Program.cs
@@ -38,13 +38,50 @@ class Program
     static void Main()
     {
 
-        Person person = new Person();
 
-        Console.Write("Skriv ditt namn: ");
-        person.userName = Console.ReadLine();
-        profiles.Add(person);
-        Console.Write("Skriv in din Ålder: ");
-        person.age = int.Parse(Console.ReadLine());
+        while (true)
+        {
+
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("Välj ett av följande val: ");
+            Console.WriteLine("1: Skapa en Profil ");
+            Console.WriteLine("2: Se alla profiler: ");
+            Console.WriteLine("3: Avsluta");
+            Console.WriteLine("-------------");
+            string answer = Console.ReadLine();
+
+            if (answer == "1")
+            {
+
+                Person person = new Person();
+
+                Console.Write("Skriv ditt namn: ");
+                person.userName = Console.ReadLine();
+                profiles.Add(person);
+                Console.Write("Skriv in din Ålder: ");
+                person.age = int.Parse(Console.ReadLine()!);
+
+
+            }
+
+            else if (answer == "2")
+            {
+                foreach (Person p in profiles)
+                {
+                    Console.WriteLine($"Namn: {p.userName} Ålder: {p.age} ");
+                }
+            }
+            else
+            {
+                break;
+            }
+
+
+
+        }
+
+
 
 
 
